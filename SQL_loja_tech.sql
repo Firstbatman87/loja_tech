0c898bc048966eb6dc3e0942dcd5ec6c57805a6f
FirstBatman87 <sidneymiguel999@gmail.com>
2026-08-20 15:49:58 -0300

    Update SQL_loja_tech.sql

--- SQL_loja_tech.sql
+++ SQL_loja_tech.sql
@@ -2,10 +2,10 @@ create database loja_tech;
 use loja_tech;
 
 create table cliente (
-id_cliente int auto_increment primary key,
+id_cliente int primary key auto_increment,
 nome varchar(100) not null,
 cpf varchar(14) not null unique,
-email varchar(100) unique not null,
+email varchar(100) not null unique,
 telefone varchar(16) not null unique,
 data_nascimento date not null,
 cidade varchar(50) not null,
@@ -19,7 +19,7 @@ create table funcionario (
 id_funcionario int primary key auto_increment,
 nome varchar(100) not null,
 cpf varchar(14) not null unique,
-email varchar(100) unique not null,
+email varchar(100) not null unique,
 telefone varchar(16) not null unique,
 cargo varchar(50) not null,
 salario decimal(10,2) not null,
@@ -28,3 +28,54 @@ data_admissao date not null
 
 show tables;
 
+create table fornecedor (
+id_fornecedor int primary key auto_increment,
+razao_social varchar(150) not null,
+nome_fantasia varchar(100) not null,
+cnpj varchar(18) not null unique,
+email varchar(50) not null unique,
+telefone varchar(16) not null unique,
+cidade varchar(50) not null,
+estado char(2) not null
+);
+
+show tables;
+
+create table categoria (
+id_categoria int primary key auto_increment,
+nome varchar(100) not null unique,
+descricao varchar(255) not null
+);
+
+show tables;
+
+create table produto (
+id_produto int primary key auto_increment,
+id_categoria int not null,
+id_fornecedor int not null,
+nome varchar(150) not null,
+dercicao text,
+preco decimal(10, 2) not null,
+custo decimal(10, 2) not null,
+codigo_barras varchar(30) not null unique,
+marca varchar(100) not null,
+modelo varchar(100) not null,
+data_cadastro date not null,
+
+foreign key (id_categoria) references categoria(id_categoria),
+foreign key (id_fornecedor) references fornecedor(id_fornecedor)
+);
+
+show tables;
+
+create table estoque (
+id_estoque int primary key auto_increment,
+id_produto int not null unique,
+quantidade int not null default 0,
+estoque_minimo int not null default 5,
+ultima_atualizacao datetime not null,
+
+foreign key (id_produto) references produto(id_produto)
+);
+
+show tables;
